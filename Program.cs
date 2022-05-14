9cffb6f361b3fe790ac606df1dcc357806c96e12
JoaoPedrocordoba <jp.8pedro862gmail.com>
2022-05-14 11:16:31 -0300

    finalizado exercicio

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int n1,n2, Contador = 1;
+
+Console.Write("Tabuada do número: ");
+n1 =  Convert.ToInt32(Console.ReadLine());
+
+n2 = 0;
+
+while (n2 < 10 )
+{
+    n2 = n2 + 1;
+    Contador =  n1 * n2;
+    Console.WriteLine($"{n1} x {n2} = {Contador}");
+}
